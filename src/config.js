041ed23622ad812dc17abf10c93a2997ac30a586
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-08 00:34:46 -0700

    Finalizes React app setup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "dlp-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://kcif0tk0e9.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_XYoo8NVQe",
+    APP_CLIENT_ID: "5837j58f57qs2qf7r5nrq46mf3",
+    IDENTITY_POOL_ID: "us-east-2:965bf91d-d03d-4938-8d13-3a70a398edb4"
+  }
+};
