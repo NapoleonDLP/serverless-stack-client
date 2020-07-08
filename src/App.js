b962d618c2aa836e53ec5ac476eed0d8e4d9b19a
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-08 14:41:32 -0700

    Conditionally renders based on authentication

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Nav, Navbar, NavItem } from "react-bootstrap";
 import "./App.css";
 import Routes from "./Routes";
 import { LinkContainer } from "react-router-bootstrap";
 import { AppContext } from "./libs/contextLib";
+import { Auth } from "aws-amplify";
+
 
 
 function App() {
+  const [isAuthenticating, setIsAuthenticating] = useState(true);
   const [isAuthenticated, userHasAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await Auth.currentSession();
+      userHasAuthenticated(true);
+    }
+    catch(e) {
+      if (e !== 'No current user') {
+        alert(e);
+      }
+    }
+
+    setIsAuthenticating(false);
+  }
 
   function handleLogout() {
     userHasAuthenticated(false);
   }
 
   return (
+    !isAuthenticating &&
     <div className="App container">
       <Navbar fluid collapseOnSelect>
         <Navbar.Header>
@@ -23,7 +45,7 @@ function App() {
           </Navbar.Brand>
           <Navbar.Toggle />
         </Navbar.Header>
-          <Navbar.Collapse>
+        <Navbar.Collapse>
           <Nav pullRight>
             {isAuthenticated
               ? <NavItem onClick={handleLogout}>Logout</NavItem>
@@ -39,7 +61,9 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-      <AppContext.Provider value={{ isAuthenticated, userHasAuthenticated }}>
+      <AppContext.Provider
+        value={{ isAuthenticated, userHasAuthenticated }}
+      >
         <Routes />
       </AppContext.Provider>
     </div>
