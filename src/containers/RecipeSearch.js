e30f9006c69a5d5617e29502d2d903ce1c82af7b
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-27 02:03:05 -0700

    Renders mock title and img

--- src/containers/RecipeSearch.js
+++ src/containers/RecipeSearch.js
@@ -1,37 +1,55 @@
 import React, { useState, useEffect } from "react";
 import sampleRecipes from "../mocks/recipes.js";
+// import { useAppContext } from "../libs/contextLib";
+// import { onError } from "../libs/errorLib";
+import { Carousel } from 'react-bootstrap';
 
 
 export default function RecipeSearch() {
   // update state by api call
   // have a section to enter what you have in the fridge
-  const [recipes, setRecipes] = useState([]);
+  // const [recipes, setRecipes] = useState([]);
+  // const { isAuthenticated } = useAppContext();
+  // const [isLoading, setIsLoading] = useState(true);
 
   // useEffect(() => {
-  var retrieveRecipes = function() {
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query="pasta"&addRecipeInformation=true&number=5`)
-    .then((data) => data.json())
-    .then((recipes) => {
-      setRecipes(recipes.results);
-    })
-  }
-  // });
+  //   async function onLoad() {
+  //     if (!isAuthenticated) {
+  //       return;
+  //     }
 
-  console.log("Recipes After Fetch:", recipes)
+  //     try {
+  //       const recipes = await retrieveRecipes();
+  //       setRecipes(recipes.results);
+  //     } catch (e) {
+  //       onError(e);
+  //     }
+
+  //     setIsLoading(false);
+  //   }
+
+  //   onLoad();
+  // }, [isAuthenticated]);
+
+  // var retrieveRecipes = function() {
+  //   return fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_SPOONACULAR_API_KEY}&query="pasta"&addRecipeInformation=true&number=5`)
+  //   .then((data) => data.json())
+  //   // .then((recipes) => {
+  //   //   setRecipes(recipes.results);
+  //   // })
+  // }
+
+  // console.log("Recipes after fetch:", recipes)
 
   return (
     <div className="recipes">
-      {sampleRecipes.map((recipe) => {
-        console.log("Link to image:", recipe.image)
-        return (
-          <div className="recipe">
-            {/* <button> */}
+        {sampleRecipes.map((recipe) => {
+          return (
+          <div onClick={() => console.log(`${recipe.title} was clicked!`)} className="recipe">
               <h2>{recipe.title}</h2>
               <img class="recipe-image" alt="" src={`${recipe.image}`}></img>
-            {/* </button> */}
           </div>
-        )
-      })}
+        )})}
     </div>
   )
 }
