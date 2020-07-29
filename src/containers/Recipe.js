45d4d6c9d8fd050064e3329e47651b952b88defd
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-29 00:10:27 -0700

    Added Ingredients and Instructions panel

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Badge } from "react-bootstrap";
+import { Badge, Panel, PanelGroup, ListGroup, ListGroupItem } from "react-bootstrap";
 
 export default function Recipe() {
   var history = useHistory();
@@ -8,10 +8,63 @@ export default function Recipe() {
   console.log("Recipe Data:", recipe)
   return (
     <div>
-      <h1>{`${recipe.title}`}</h1>
-      <img alt={recipe.title} src={`${recipe.image}`}></img>
-      <p>{recipe.summary}</p>
-      {console.log(recipe.extendedIngredients)}
+      <Panel>
+        <Panel.Heading>
+          <Panel.Title componentClass="H1"><h2>{recipe.title}</h2></Panel.Title>
+        </Panel.Heading>
+
+        <Panel.Body>
+          <div>
+            <img alt={recipe.title} src={`${recipe.image}`}></img>
+          </div>
+
+          <PanelGroup
+            accordion
+            id="accordion-controlled-ingredients"
+            >
+
+            <Panel>
+            <Panel.Heading>
+              <Panel.Title toggle><h3>Ingredients</h3></Panel.Title>
+            </Panel.Heading>
+            <Panel.Body collapsible>
+              <ListGroup>
+                {recipe.extendedIngredients.map((ingredient) => {
+                  return(
+                    <ListGroupItem>{ingredient.original}</ListGroupItem>
+                  )
+                })}
+              </ListGroup>
+            </Panel.Body>
+            </Panel>
+          </PanelGroup>
+
+          <PanelGroup
+            accordion
+            id="accordion-controlled-instructions"
+            >
+
+            <Panel>
+            <Panel.Heading>
+              <Panel.Title toggle><h3>Instructions</h3></Panel.Title>
+            </Panel.Heading>
+            <Panel.Body collapsible>
+              <ListGroup>
+                {recipe.analyzedInstructions[0].steps.map((instruction, i) => {
+                  return(
+                    <ListGroupItem>
+                      {`Step ${i +1}: ${instruction.step}`}
+                    </ListGroupItem>
+                  )
+                })}
+              </ListGroup>
+            </Panel.Body>
+            </Panel>
+          </PanelGroup>
+
+        </Panel.Body>
+
+      </Panel>
       <p>
         <Badge>{recipe.aggregateLikes}</Badge> Likes
       </p>
