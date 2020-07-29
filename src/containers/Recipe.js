3a534abdc6083d3da8ef74c40f41d8bee866243b
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-29 01:55:53 -0700

    Defaults panels to collapsed

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { Badge, Panel, PanelGroup, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Badge, Panel, ListGroup, ListGroupItem } from "react-bootstrap";
+
 
 export default function Recipe() {
   var history = useHistory();
   var recipe = history.location.state;
-  console.log("Recipe Data:", recipe)
+
   return (
     <div>
       <Panel>
         <Panel.Heading>
-          <Panel.Title componentClass="H1"><h2>{recipe.title}</h2></Panel.Title>
+          <Panel.Title><h2>{recipe.title}</h2></Panel.Title>
         </Panel.Heading>
 
         <Panel.Body>
@@ -18,31 +19,21 @@ export default function Recipe() {
             <img alt={recipe.title} src={`${recipe.image}`}></img>
           </div>
 
-          <PanelGroup
-            accordion
-            id="accordion-controlled-ingredients"
-            >
-
             <Panel>
             <Panel.Heading>
               <Panel.Title toggle><h3>Ingredients</h3></Panel.Title>
             </Panel.Heading>
+
             <Panel.Body collapsible>
               <ListGroup>
-                {recipe.extendedIngredients.map((ingredient) => {
+                {recipe.extendedIngredients.map((ingredient, i) => {
                   return(
-                    <ListGroupItem>{ingredient.original}</ListGroupItem>
+                    <ListGroupItem key={i}>{ingredient.original}</ListGroupItem>
                   )
                 })}
               </ListGroup>
             </Panel.Body>
             </Panel>
-          </PanelGroup>
-
-          <PanelGroup
-            accordion
-            id="accordion-controlled-instructions"
-            >
 
             <Panel>
             <Panel.Heading>
@@ -52,7 +43,7 @@ export default function Recipe() {
               <ListGroup>
                 {recipe.analyzedInstructions[0].steps.map((instruction, i) => {
                   return(
-                    <ListGroupItem>
+                    <ListGroupItem key={i}>
                       {`Step ${i +1}: ${instruction.step}`}
                     </ListGroupItem>
                   )
@@ -60,7 +51,6 @@ export default function Recipe() {
               </ListGroup>
             </Panel.Body>
             </Panel>
-          </PanelGroup>
 
         </Panel.Body>
 
