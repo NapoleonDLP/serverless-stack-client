680a3047a07f5bb0719453e7947d4ab165441d9a
NapoleonDLP <napoleon.dlp@gmail.com>
2020-07-28 22:59:30 -0700

    Added likes icon

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { Badge } from "react-bootstrap";
 
 export default function Recipe() {
   var history = useHistory();
   var recipe = history.location.state;
+  console.log("Recipe Data:", recipe)
   return (
     <div>
       <h1>{`${recipe.title}`}</h1>
       <img alt={recipe.title} src={`${recipe.image}`}></img>
       <p>{recipe.summary}</p>
       {console.log(recipe.extendedIngredients)}
+      <p>
+        <Badge>{recipe.aggregateLikes}</Badge> Likes
+      </p>
     </div>
   )
 }
