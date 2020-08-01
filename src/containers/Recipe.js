c93c4def5ee1fac021c49a15400d108b8adc464c
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-01 03:02:11 -0700

    Restyled recipe component and added recipe summary

--- src/containers/Recipe.js
+++ src/containers/Recipe.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React from "react"
 import { useHistory } from "react-router-dom";
 import { Badge, Panel, ListGroup, ListGroupItem } from "react-bootstrap";
-import "./Recipe.css"
-
+import "./Recipe.css";
 
 export default function Recipe() {
   var history = useHistory();
   var recipe = history.location.state;
 
   return (
-    <div>
-      <Panel>
-        <Panel.Heading>
-          <Panel.Title><h2>{recipe.title}</h2></Panel.Title>
-        </Panel.Heading>
-
+      <Panel className="recipe-panel">
         <Panel.Body>
-          <div>
-            <img width={"100%"} height={"100%"} alt={recipe.title} src={`${recipe.image}`}></img>
+          <div className="title-image">
+            <div id="recipe-title">
+              <Panel.Title><h2>{recipe.title}</h2></Panel.Title>
+              <h6 className="author">{`By ${recipe.sourceName}`}</h6>
+              <p id="summary">{recipe.summary.replace(/(<([^>]+)>)/ig, '')}</p>
+            </div>
+
+            <div id="recipe-image">
+              <img width={"100%"} height={"100%"} alt={recipe.title} src={`${recipe.image}`}></img>
+            </div>
           </div>
 
-            <Panel>
+          <Panel>
             <Panel.Heading>
               <Panel.Title toggle><h3>Ingredients</h3></Panel.Title>
             </Panel.Heading>
-
             <Panel.Body collapsible>
               <ListGroup>
                 {recipe.extendedIngredients.map((ingredient, i) => {
@@ -34,30 +35,34 @@ export default function Recipe() {
                 })}
               </ListGroup>
             </Panel.Body>
-            </Panel>
+          </Panel>
 
-            <Panel>
+          <Panel>
             <Panel.Heading>
               <Panel.Title toggle><h3>Instructions</h3></Panel.Title>
             </Panel.Heading>
             <Panel.Body collapsible>
               <ListGroup>
-                {recipe.analyzedInstructions[0].steps.map((instruction, i) => {
+                {recipe.analyzedInstructions[0].steps.map(  (instruction, i) => {
                   return(
                     <ListGroupItem key={i}>
                       {`Step ${i +1}: ${instruction.step}`}
                     </ListGroupItem>
-                  )
-                })}
+                  )})}
               </ListGroup>
             </Panel.Body>
-            </Panel>
+          </Panel>
 
         </Panel.Body>
-        <div id="likes">
-          <Badge>{recipe.aggregateLikes}</Badge> Likes
+
+        <div className="tags-container">
+          <div id="likes">
+            <Badge className="aggregateLikes">{recipe.aggregateLikes}</Badge>
+            <p className="aggregateLikes">Likes</p>
+          </div>
+            <p className="tags">Dairy free</p>
         </div>
+
       </Panel>
-    </div>
   )
 }
