0672659456d772349e7b09484dae1301556da4ec
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-06 03:18:09 -0700

    Removed notes render, added saved recipes render

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+// import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
 import { API } from "aws-amplify";
-import { LinkContainer } from "react-router-bootstrap";
+// import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import RecipeSearchContainer from "./RecipeSearch.js";
 import RecipeRecommendation from "./Recommendation.js";
+import SavedRecipes from "./SavedRecipes.js";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
-  const [searchTerm, setSearchTerm] = useState([])
+  // const [searchTerm, setSearchTerm] = useState([]);
   const { isAuthenticated } = useAppContext();
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function onLoad() {
@@ -28,36 +29,39 @@ export default function Home() {
         onError(e);
       }
 
-      setIsLoading(false);
+      // setIsLoading(false);
     }
 
     onLoad();
   }, [isAuthenticated]);
 
-  function loadNotes() {
-    return API.get("notes", "/notes");
+  async function loadNotes() {
+    return await API.get("notes", "/notes");
   }
 
-  function renderNotesList(notes) {
-
-    return [{}].concat(notes).map((note, i) =>
-      i !== 0 ? (
-        <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
-          <ListGroupItem header={note.content.trim().split("\n")[0]}>
-            {"Created: " + new Date(note.createdAt).toLocaleString()}
-          </ListGroupItem>
-        </LinkContainer>
-      ) : (
-        <LinkContainer key="new" to="/notes/new">
-          <ListGroupItem>
-            <h4>
-              <b>{"\uFF0B"}</b> Create a new note
-            </h4>
-          </ListGroupItem>
-        </LinkContainer>
-      )
-    );
+  function filterRecipes(notes) {
+    return notes.filter((recipe) => recipe.recipe !== undefined)
   }
+
+  // function renderNotesList(notes) {
+  //   return [{}].concat(notes).map((note, i) =>
+  //     i !== 0 ? (
+  //       <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+  //         <ListGroupItem header={note.content ? note.content.trim().split("\n")[0] : null}>
+  //           {"Created: " + new Date(note.createdAt).toLocaleString()}
+  //         </ListGroupItem>
+  //       </LinkContainer>
+  //     ) : (
+  //       <LinkContainer key="new" to="/notes/new">
+  //         <ListGroupItem>
+  //           <h4>
+  //             <b>{"\uFF0B"}</b> Create a new note
+  //           </h4>
+  //         </ListGroupItem>
+  //       </LinkContainer>
+  //     )
+  //   );
+  // }
 
   function renderLander() {
     return (
@@ -76,50 +80,50 @@ export default function Home() {
     );
   }
 
-  function handleInputChange(e) {
-    let searchResults = [];
+  // function handleInputChange(e) {
+  //   let searchResults = [];
 
-    for (var i = 0; i < notes.length; i++) {
-      var lowerCaseContent = notes[i].content.toLowerCase()
-      if (lowerCaseContent.includes(e.target.value.toLowerCase())) {
-        searchResults.push(notes[i]);
-      }
-      setSearchTerm(searchResults);
-    }
-  }
+  //   for (var i = 0; i < notes.length; i++) {
+  //     var lowerCaseContent = notes[i].content.toLowerCase()
+  //     if (lowerCaseContent.includes(e.target.value.toLowerCase())) {
+  //       searchResults.push(notes[i]);
+  //     }
+  //     setSearchTerm(searchResults);
+  //   }
+  // }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+  // function handleSubmit(e) {
+  //   e.preventDefault();
+  // }
 
-  function renderNotes() {
-    return (
-      <div className="notes">
-        <PageHeader>Your Saved Recipes</PageHeader>
-          <div name="search bar">
-            <form onSubmit={handleSubmit}>
-              <label>
-              <input placeholder="&#xF002; Search" className="fontAwesome" onChange={handleInputChange} type="text" name ></input>
-              </label>
-            </form>
+  // function renderNotes() {
+  //   return (
+  //     <div className="notes">
+  //       <PageHeader>Your Saved Recipes</PageHeader>
+  //         <div name="search bar">
+  //           <form onSubmit={handleSubmit}>
+  //             <label>
+  //             <input placeholder="&#xF002; Search" className="fontAwesome" onChange={handleInputChange} type="text" name ></input>
+  //             </label>
+  //           </form>
 
-        </div>
-        <ListGroup>
-          {!isLoading && searchTerm.length === 0 ? renderNotesList(notes) : renderNotesList(searchTerm)}
-        </ListGroup>
-      </div>
-    );
-  }
+  //       </div>
+  //       <ListGroup>
+  //         {!isLoading && searchTerm.length === 0 ? renderNotesList(notes) : renderNotesList(searchTerm)}
+  //       </ListGroup>
+  //     </div>
+  //   );
+  // }
 
   return (
     <div className="home-page">
     <div className="Home">
-      <RecipeRecommendation />
+      <RecipeRecommendation savedRecipes={filterRecipes(notes)}/>
     <div className="recipe-search-container">
-      <RecipeSearchContainer />
+      <RecipeSearchContainer savedRecipes={filterRecipes(notes)}/>
     </div>
       <div>
-        {isAuthenticated ? renderNotes() : renderLander()}
+        {isAuthenticated ? <SavedRecipes savedRecipes={filterRecipes(notes)}/> : renderLander()}
       </div>
     </div>
     </div>
