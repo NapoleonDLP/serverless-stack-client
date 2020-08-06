e0a817b8c9ec45f91151549484a95392e6126ec3
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-06 03:16:41 -0700

    Changes colors of heart when clicked. Saves to recipe to DB

--- src/components/HeartButton.js
+++ src/components/HeartButton.js
@@ -1,20 +1,94 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Glyphicon } from "react-bootstrap";
 import "./HeartButton.css";
+import { API } from "aws-amplify";
+import { onError } from "../libs/errorLib";
 
 export default function Heart (props) {
+  const currentRecipe = props.recipe;
+  const [ savedRecipes, setSavedRecipes] = useState(props.savedRecipes);
   const [ saved, setSaved] = useState(false);
-  // Take recipe that was clicked on
-  // Save it to saved recipes list on return update saved recipes
-  // render div
-  var saveRecipe = function() {
+
+  useEffect(() => {
+    async function onLoad() {
+      try {
+        let found = await isSaved()
+        setSaved(found);
+        setSavedRecipes(props.savedRecipes);
+      } catch(e) {
+        console.log("Error in HeartButton:", e)
+        // console.log("Current", currentRecipe, "Saved:", savedRecipes)
+        // onError(e)
+      }
+    }
+
+    onLoad();
+  });
+
+  function isSaved() {
+    var currId = currentRecipe.recipe.id === undefined ? currentRecipe.recipeId : currentRecipe.recipe.id;
+    for (var i = 0; i < savedRecipes.length; i++) {
+      // console.log("each saved:",savedRecipes[i].recipeId, "current:", currentRecipe.recipeId || currentRecipe.id)
+      if (savedRecipes[i].recipeId === currId) {
+        setSaved(true);
+        return true;
+      }
+    }
+    setSaved(false);
+    return false;
+  }
+
+  function deleteRecipe() {
+    console.log("Delete Recipe:", currentRecipe.noteId)
+
+    return API.del("notes", `/notes/${currentRecipe.noteId}`);
+  }
+
+  // function logRecipe() {
+  //   console.log('CURR CURR id:', currentRecipe.id, "CUR CUR recipeId", currentRecipe.recipeId, "saved", savedRecipes)
+  // }
+
+  function saveRecipe() {
+    let savedNotes = null;
+    let savedImage = null;
+    let recipeId = currentRecipe.id;
+    let recipe = currentRecipe;
     setSaved(!saved);
+    return API.post("notes", "/saveRecipe", {
+      body: {savedNotes, savedImage, recipeId, recipe}
+    })}
+
+    async function handleDelete (event) {
+      // event.preventDefault();
+      console.log("EVENT", event)
+
+      try {
+        await deleteRecipe();
+        // history.push("/")
+      } catch (e) {
+        console.log("ERROR:", e)
+        // onError(e);
+      }
+    }
+
+  async function handleSave (event) {
+    // event.preventDefault();
+    console.log("EVENT", event)
+
+    try {
+      await saveRecipe();
+      // history.push("/")
+    } catch (e) {
+      console.log("ERROR:", e)
+      onError(e);
+    }
   }
 
   return (
-    <div className={saved ? "red" : "orange"}>
+    <div className={ saved ? "red" : "orange"}>
       <Glyphicon
-        onClick={ saveRecipe }
+        onClick={ saved ? handleDelete : handleSave }
+        // onClick={logRecipe}
         bsClass="glyphicon"
         glyph="heart" />
     </div>
