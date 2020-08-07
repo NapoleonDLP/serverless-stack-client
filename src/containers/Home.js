15cc30c85e0ace3fffa1347bcdd8064cdb7b45e0
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-07 05:05:09 -0700

    Cleaned up comments removed old code

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
-// import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { useAppContext } from "../libs/contextLib";
 import { onError } from "../libs/errorLib";
 import "./Home.css";
 import { API } from "aws-amplify";
-// import { LinkContainer } from "react-router-bootstrap";
 import { Link } from "react-router-dom";
 import RecipeSearchContainer from "./RecipeSearch.js";
 import RecipeRecommendation from "./Recommendation.js";
@@ -12,9 +10,7 @@ import SavedRecipes from "./SavedRecipes.js";
 
 export default function Home() {
   const [notes, setNotes] = useState([]);
-  // const [searchTerm, setSearchTerm] = useState([]);
   const { isAuthenticated } = useAppContext();
-  // const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function onLoad() {
@@ -26,10 +22,8 @@ export default function Home() {
         const notes = await loadNotes();
         setNotes(notes);
       } catch (e) {
-        onError(e);
+        console.error(e)
       }
-
-      // setIsLoading(false);
     }
 
     onLoad();
@@ -42,26 +36,6 @@ export default function Home() {
   function filterRecipes(notes) {
     return notes.filter((recipe) => recipe.recipe !== undefined)
   }
-
-  // function renderNotesList(notes) {
-  //   return [{}].concat(notes).map((note, i) =>
-  //     i !== 0 ? (
-  //       <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
-  //         <ListGroupItem header={note.content ? note.content.trim().split("\n")[0] : null}>
-  //           {"Created: " + new Date(note.createdAt).toLocaleString()}
-  //         </ListGroupItem>
-  //       </LinkContainer>
-  //     ) : (
-  //       <LinkContainer key="new" to="/notes/new">
-  //         <ListGroupItem>
-  //           <h4>
-  //             <b>{"\uFF0B"}</b> Create a new note
-  //           </h4>
-  //         </ListGroupItem>
-  //       </LinkContainer>
-  //     )
-  //   );
-  // }
 
   function renderLander() {
     return (
@@ -79,41 +53,6 @@ export default function Home() {
       </div>
     );
   }
-
-  // function handleInputChange(e) {
-  //   let searchResults = [];
-
-  //   for (var i = 0; i < notes.length; i++) {
-  //     var lowerCaseContent = notes[i].content.toLowerCase()
-  //     if (lowerCaseContent.includes(e.target.value.toLowerCase())) {
-  //       searchResults.push(notes[i]);
-  //     }
-  //     setSearchTerm(searchResults);
-  //   }
-  // }
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  // }
-
-  // function renderNotes() {
-  //   return (
-  //     <div className="notes">
-  //       <PageHeader>Your Saved Recipes</PageHeader>
-  //         <div name="search bar">
-  //           <form onSubmit={handleSubmit}>
-  //             <label>
-  //             <input placeholder="&#xF002; Search" className="fontAwesome" onChange={handleInputChange} type="text" name ></input>
-  //             </label>
-  //           </form>
-
-  //       </div>
-  //       <ListGroup>
-  //         {!isLoading && searchTerm.length === 0 ? renderNotesList(notes) : renderNotesList(searchTerm)}
-  //       </ListGroup>
-  //     </div>
-  //   );
-  // }
 
   return (
     <div className="home-page">
