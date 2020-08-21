29e7f170d41446266c50d56fa8061613099ccdbc
NapoleonDLP <napoleon.dlp@gmail.com>
2020-08-21 03:51:10 -0700

    Adds button which recommends local restaurants that offer dish

--- src/containers/Restaurants.js
+++ src/containers/Restaurants.js
@@ -1,49 +1,52 @@
 import React, {useState, useEffect} from "react";
-// import axios from "axios";
 import { API } from "aws-amplify";
 
 export default function Restaurants(props) {
-  // const recipe = props.recipe;
-  const [ restaurants, setRestaurants] = useState([]);
+  const [ coords, setCoords ] = useState(null);
+  const [ restaurants, setRestaurants] = useState(null);
 
   useEffect(() => {
     async function onLoad() {
       try {
-        let results = await retrieveRestaurants();
-        // console.log("YELP RESULTS:", results)
-        setRestaurants(results);
+        await usersLocation();
+        // let results = await retrieveRestaurants();
+        // setRestaurants(results);
       } catch (e) {
         console.log("error in restaurants:", e)
       }
     }
 
     onLoad()
-    console.log("REST:",restaurants)
-  });
 
-  function retrieveRestaurants() {
-    // let latitude = 38.9399;
-    // let longitude = -119.9772;
-    // let term = "tacos";
+  }, []);
 
-    return API.post("notes", "/restaurants", {
-      body: {latitude: 38.9399, longitude: -119.9772, term: "tacos"}
+  async function usersLocation () {
+    await navigator.geolocation.getCurrentPosition((pos) => {
+      setCoords(pos.coords);
     });
+  };
 
-    // const restaurants = await fetch(`https://kcif0tk0e9.execute-api.us-east-2.amazonaws.com/prod/restaurants`, {
-    //   // mode: "no-cors",
-    //   method: "POST",
-    //   body: JSON.stringify(data)
-    // })
-    // .then((data) => data.json())
-    // .then((data) => console.log("YELP RESULTS:", data))
-    // .catch((err) => console.log("ERROR:",err))
+  async function retrieveRestaurants() {
+    let results = await API.get("notes", `/restaurants?latitude=${coords.latitude}&longitude=${coords.longitude}&term=${props.recipe.title}`);
+    console.log("Results:", results);
+    return results;
+  }
+
+  async function updateRestaurants() {
+    let update = await retrieveRestaurants();
+    setRestaurants(update);
   }
 
   return (
     <div>
-      <button onClick={retrieveRestaurants}>YELP</button>
-      <h3>CHECK THESE OUT</h3>
+      <button onClick={updateRestaurants}>Missing an ingredient? Changed your mind about cooking tonight?</button>
+      <h3>{coords ? updateRestaurants : "LOADING..."}</h3>
+      <h3>{restaurants ? (restaurants.businesses.map((restaurant) => {
+        return (
+          <div>
+            <small>{restaurant.alias}</small>
+          </div>
+          )})) : null}</h3>
     </div>
   )
 }
